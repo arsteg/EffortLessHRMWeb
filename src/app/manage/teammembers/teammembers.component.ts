0f9080a33f63ded5daf248248903db99fd1ebcf2
dotnetexpertdev <dotnetexpertdev@gmail.com>
2022-12-12 20:12:26 +0530

    team members selection

--- src/app/manage/teammembers/teammembers.component.ts
+++ src/app/manage/teammembers/teammembers.component.ts
@@ -4,12 +4,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Subordinate } from 'src/app/models/subordinate.Model';
 import { User } from 'src/app/models/user';
-import { ManageTeamService} from 'src/app/_services/manage-team.service';
+import { ManageTeamService } from 'src/app/_services/manage-team.service';
 import { TimeLogService } from 'src/app/_services/timeLogService';
 
-export class Hero{
-id:number;
-name:string;
+export class Hero {
+  id: number;
+  name: string;
 }
 
 @Component({
@@ -17,35 +17,35 @@ name:string;
   templateUrl: './teammembers.component.html',
   styleUrls: ['./teammembers.component.css']
 })
-export class TeammembersComponent implements OnInit, OnDestroy{
-//#region Private Members
-resetToken: null;
-teamOfUsers:User[]=[];
-allUsers:User[]=[];
-selectedUsers:any;
-selectedUser: any;
-selectedManager:any;
-subscription: Subscription;
-message:[]=[];
-selectedValue:any;
+export class TeammembersComponent implements OnInit, OnDestroy {
+  //#region Private Members
+  resetToken: null;
+  teamOfUsers: User[] = [];
+  allUsers: User[] = [];
+  selectedUsers: any;
+  selectedUser: any;
+  selectedManager: any;
+  subscription: Subscription;
+  message: [] = [];
+  selectedValue: any;
 
-onSelect(user: User): void {
-  this.selectedUser = user;
-}
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
 
-//#endregion
+  //#endregion
 
-//#region Constructor
+  //#region Constructor
 
-constructor(private manageTeamService:ManageTeamService, private timeLogService:TimeLogService,
-  private route: ActivatedRoute,
-  private router: Router,
-  private datePipe: DatePipe) {
-  this.route.params.subscribe(params => {
-    this.resetToken = params['token'];
+  constructor(private manageTeamService: ManageTeamService, private timeLogService: TimeLogService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private datePipe: DatePipe) {
+    this.route.params.subscribe(params => {
+      this.resetToken = params['token'];
     });
-}
-//#endregion
+  }
+  //#endregion
   ngOnInit(): void {
     this.populateTeamOfUsers();
     this.subscription = this.timeLogService.currentMessage.subscribe(message => this.selectedValue = message)
@@ -53,67 +53,74 @@ constructor(private manageTeamService:ManageTeamService, private timeLogService:
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
-  
+
   //#region Private methods
-  populateTeamOfUsers(){
+  populateTeamOfUsers() {
     this.manageTeamService.getAllUsers().subscribe({
-    next:result=>{
-      this.teamOfUsers = result.data.data;
-      this.allUsers = result.data.data;
-      console.log("team", this.teamOfUsers)
-    },
-    error:error=>{}
+      next: result => {
+        this.teamOfUsers = result.data.data;
+        this.allUsers = result.data.data;
+        console.log("team", this.teamOfUsers)
+      },
+      error: error => { }
     })
   }
 
-  Selectmanager(event:any){
- 
-    this.timeLogService.getTeamMembers(event.target.value).subscribe({
+  Selectmanager(user: any) {
+    this.selectedManager = user;
+    this.timeLogService.getTeamMembers(user.id).subscribe({
       next: response => {
-     console.log("response", response);
+        console.log("response", response);
         this.timeLogService.getusers(response.data).subscribe({
           next: result => {
-            debugger;
-            let arr = [];
-            this.selectedValue =JSON.parse(event.target.value); 
-            arr.push(this.selectedValue)
-            console.log(arr);
-            this.timeLogService.changeMessage(arr);
+            this.selectedUsers = result.data;
+            this.teamOfUsers.forEach((user:any, index:number)=>{
+              user['isChecked'] = this.selectedUsers.some((selectedUser:any)=>selectedUser.id == user.id);
+            });
+            //  debugger;
+            // let arr = [];
+            // this.selectedValue =JSON.parse(user.target.value); 
+            // arr.push(this.selectedValue)
+            // console.log(arr);
+            // this.timeLogService.changeMessage(arr);
 
-            
+
           },
           error: error => {
-              console.log('There was an error!', error);
+            console.log('There was an error!', error);
           }
-      });
+        });
       },
       error: error => {
-          console.log('There was an error!', error);
+        console.log('There was an error!', error);
       }
-  });
+    });
   }
 
- 
+  isChecked(){
+    
+  }
 
-  onAddMember(user:User){
+
+  onAddMember(user: User) {
     let currentDate = new Date();
-    let subordinate= new Subordinate(this.selectedManager,user.id,currentDate,this.selectedManager);
+    let subordinate = new Subordinate(this.selectedManager, user.id, currentDate, this.selectedManager);
     this.manageTeamService.addSubordinate(subordinate).subscribe({
-    next: result=>{
-    },
-    error: error=>{}
-   });
+      next: result => {
+      },
+      error: error => { }
+    });
   }
 
-  onRemoveMember(user:User){
+  onRemoveMember(user: User) {
     let currentDate = new Date();
-    let subordinate= new Subordinate(this.selectedManager,user.id,currentDate,this.selectedManager);
-    this.manageTeamService.deleteSubordinate(this.selectedManager,user.id,).subscribe({
-    next: result=>{
-    },
-    error: error=>{}
-   });
+    let subordinate = new Subordinate(this.selectedManager, user.id, currentDate, this.selectedManager);
+    this.manageTeamService.deleteSubordinate(this.selectedManager, user.id,).subscribe({
+      next: result => {
+      },
+      error: error => { }
+    });
   }
-  
+
 }
 
