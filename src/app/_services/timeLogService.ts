ed731529414e78952d776f8e13849d634f23d38a
Mohamad Rafi <dotnetexpertdev@gmail.com>
2023-05-04 11:16:40 +0530

    fixed the timeshets api call issue

--- src/app/_services/timeLogService.ts
+++ src/app/_services/timeLogService.ts
@@ -99,7 +99,7 @@ export class TimeLogService{
   }
 
   geAdminTimeSheet(userId:string,fromDate:string,toDate:string ): any {
-    var response  = this.http.post<any>(`${environment.apiUrlDotNet}/timelogs/timesheets`, {"userIds" :userId, "fromDate":fromDate, "toDate":toDate},this.httpOptions);
+    var response  = this.http.post<any>(`${environment.apiUrlDotNet}/timelogs/timesheets`, {"userIds" :userId, "startDate":fromDate, "endDate":toDate},this.httpOptions);
     return response;
   }
   //mostPopularBook: Book = allBooks[0];
