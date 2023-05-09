e777e8571b81de6013673c829a59c79fdf9b8439
Mohamad Rafi <dotnetexpertdev@gmail.com>
2023-05-09 11:37:28 +0530

    EH-85 [UI] - Generic Settings - Design
    EH-85 [UI] - Generic Settings - Design

--- src/app/settings/generic-settings/generic-settings.component.ts
+++ src/app/settings/generic-settings/generic-settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-generic-settings',
@@ -8,37 +9,63 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GenericSettingsComponent implements OnInit {
 
+  settingsByCategory: Control[];
   jsonData: any=[];
-  selectedCategory: string='0';
-  selectedJsonData: any=[];
+  selectedCategory: string='';
   categoriesData: any=[];
 
   constructor(private http: HttpClient) {
-    // this.categoriesData = this.jsonData.Category.map(c => c.CategoryName);
-    // console.log(this.categoriesData);
+
    }
 
   ngOnInit(): void {
-    debugger;
-    this.http.get('../../../assets/genericSettings.json').subscribe(data => {
+    this.http.get<Controls>('../../../assets/genericSettings.json').subscribe(data => {
       this.jsonData = data;
       this.categoriesData = [...new Set(this.jsonData.Category.map(c => c.CategoryName))];
       console.log(this.categoriesData);
     });
   }
 
-  OnCategoryChange(){
-    // if(this.selectedCategory.toLowerCase() == "profile settings"){
-    //   this.selectedJsonData = this.jsonData.Category.filter(cat=> cat.CategoryName.toLowerCase() == "profile settings")
-    // }
-    // else if (this.selectedCategory.toLowerCase() == "workspace"){
-    //   this.selectedJsonData = this.jsonData.Category.filter(cat=> cat.CategoryName.toLowerCase() == "workspace")
-    // }
-    // else{
-    //   this.selectedJsonData = [];
-    // }
-
-    this.selectedJsonData = this.jsonData.Category.filter(cat=> cat.CategoryName.toLowerCase() == this.selectedCategory.toLowerCase())
+  OnCategoryChange(event: MatTabChangeEvent){
+    this.selectedCategory = this.categoriesData[event.index];
+    this.settingsByCategory = this.jsonData.Category.filter(cat=> cat.CategoryName.toLowerCase() == this.selectedCategory.toLowerCase())
   }
 
+  saveForm() {
+    console.log(JSON.stringify(this.settingsByCategory));
+  }
+
+  updateValue(eventtype: any, key: string, fieldname: string, categoryName: string) {
+    const checked = eventtype.target.checked;
+    
+    this.settingsByCategory = this.settingsByCategory.map(cat => {
+      if (cat.FieldName.toLowerCase() === fieldname.toLowerCase()) {
+        if (checked) {
+          cat.Value.push(key);
+        }
+        else{
+          const index = cat.Value.indexOf(key);
+          if (index > -1) {
+            cat.Value.splice(index, 1);
+          }
+        }
+      }
+      return cat;
+    });
+  }
+
+}
+
+interface Control {
+  CategoryName: string;
+  ControlType: string;
+  ControlLabel: string;
+  ToolTip: string;
+  FieldName: string;
+  Value: any;
+  ListData: { key: string, value: string }[];
+}
+
+interface Controls {
+  controls: Control[];
 }
