98023c29f58ad7b59a82b1a37ee2ffc69bd158ff
kalpanakulkarni085 <133846584+kalpanakulkarni085@users.noreply.github.com>
2023-06-17 14:16:48 +0530

    Update emailtemplate.service.ts
    cors issue checking

--- src/app/_services/emailtemplate.service.ts
+++ src/app/_services/emailtemplate.service.ts
@@ -14,7 +14,8 @@ export class EmailtemplateService {
   private readonly httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'https://effortlesshrmapi.azurewebsites.net/api/v1',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Method': 'GET',
       'Authorization': `Bearer ${this.token}`
     }),
     withCredentials: true
