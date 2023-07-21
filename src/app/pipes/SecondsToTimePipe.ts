ea873a0a739500d7a86dbb4657827c2763f1a95e
Mohamad Rafi <dotnetexpertdev@gmail.com>
2023-07-21 13:02:29 +0530

    milliSecondsToTime changes

--- src/app/pipes/SecondsToTimePipe.ts
+++ src/app/pipes/SecondsToTimePipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MilliSecondsToTimePipe implements PipeTransform {
   transform(totalSeconds: number): string {
     const hours = Math.floor(totalSeconds / 3600000);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    if(hours>0){
+    totalSeconds = totalSeconds - (hours*60*60*1000)
+   }
+    const minutes = Math.floor((totalSeconds/1000/60) << 0);
     const hoursString = hours.toString().padStart(2, '0');
     const minutesString = minutes.toString().padStart(2, '0');
     return `${hoursString}:${minutesString}`;
