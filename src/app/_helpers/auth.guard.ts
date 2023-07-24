3212e326b6d3825fd285cf689dcbbe32e2f13312
dotnetexpertdev <dotnetexpertdev@gmail.com>
2023-07-24 16:56:35 +0530

    Revert "added another logic for cookies"
    This reverts commit 326f2f9189d1ecf46e6a252dae6dc917ce3deb14.

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthenticationService } from '../_services/authentication.service';
 
@@ -14,22 +14,21 @@ export class AuthGuard implements CanActivate {
         private authenticationService: AuthenticationService
     ) {}
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-        const companyIdCookie = this.getCookie('companyId');
-        const userIdCookie = this.getCookie('userId');
-    
-        if (companyIdCookie && userIdCookie) {
-          return true;
-        } else {
-          // If cookies are not present, redirect to the login page
-          return this.router.parseUrl('/login');
+    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) : Observable<boolean> {
+        return this.authenticationService.isLoggedIn // {1}
+        .pipe(
+        take(1), // {2}
+        map((isLoggedIn: boolean) => { // {3}
+
+        if (!isLoggedIn){
+
+        //this.router.navigate(['/login']); // {4}
+        return true;
         }
-      }
-    
-      private getCookie(name: string): string {
-        const value = "; " + document.cookie;
-        const parts = value.split("; " + name + "=");
-        if (parts.length == 2) return parts.pop()?.split(";").shift() || "";
-        return "";
-      }
+
+        return true;
+        })
+        )
+
+    }
 }
