e5080da6a7430439014ff4b65a708d7c62feba41
dotnetexpertdev <dotnetexpertdev@gmail.com>
2023-07-24 16:34:10 +0530

    Revert "Revert change for test"
    This reverts commit 009db5c4eb20d2386c26d2872f8ae4a49f2e7942.

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -9,32 +9,32 @@ import { AuthenticationService } from './_services/authentication.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthenticationService, private router: Router) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (this.authService.isLoggedIn()) {
-        return true;
-      } else {
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-        return false;
-      }
+  // canActivate(
+  //   next: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //     if (this.authService.isLoggedIn()) {
+  //       return true;
+  //     } else {
+  //       this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+  //       return false;
+  //     }
+  // }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    const companyIdCookie = this.getCookie('companyId');
+    const userIdCookie = this.getCookie('userId');
+
+    if (companyIdCookie && userIdCookie) {
+      return true;
+    } else {
+      // If cookies are not present, redirect to the login page
+      return this.router.parseUrl('/login');
+    }
   }
-  // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
-  //   const companyIdCookie = this.getCookie('companyId');
-  //   const userIdCookie = this.getCookie('userId');
 
-  //   if (companyIdCookie && userIdCookie) {
-  //     return true;
-  //   } else {
-  //     // If cookies are not present, redirect to the login page
-  //     return this.router.parseUrl('/login');
-  //   }
-  // }
-
-  // private getCookie(name: string): string {
-  //   const value = "; " + document.cookie;
-  //   const parts = value.split("; " + name + "=");
-  //   if (parts.length == 2) return parts.pop()?.split(";").shift() || "";
-  //   return "";
-  // }
+  private getCookie(name: string): string {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop()?.split(";").shift() || "";
+    return "";
+  }
 }
