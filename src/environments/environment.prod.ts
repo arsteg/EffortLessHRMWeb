7627c9dd2dbbc6aed24aa1170653816b8a1e7a40
dotnetexpertdev <dotnetexpertdev@gmail.com>
2023-09-19 15:01:09 +0530

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-let apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com";
+let apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/api/v1";
 if(window.location.host.includes('localhost'))
 {
-  apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com";
+  apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/api/v1";
 }
 export const environment = {
   production: true,
