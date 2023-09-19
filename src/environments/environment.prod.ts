024081a721f58a9117f6b101a5b43f999d26d8d6
dotnetexpertdev <dotnetexpertdev@gmail.com>
2023-09-19 14:37:25 +0530

    Updated URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-let apiUrlDotNet="https://ehrmapi.azurewebsites.net/api/v1";
+let apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/";
 if(window.location.host.includes('localhost'))
 {
-  apiUrlDotNet="https://ehrmapi.azurewebsites.net/api/v1/";
+  apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/";
 }
 export const environment = {
   production: true,
