a143e5ab709cd33a2b983729b7d1b6ba3837ad92
dotnetexpertdev <dotnetexpertdev@gmail.com>
2023-09-19 15:35:39 +0530

    Revert "test"
    This reverts commit 6a7a81b488e16d6fcdb3b14cab12b1d709dc6379.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-let apiUrlDotNet="https://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/api/v1";
+let apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/api/v1";
 if(window.location.host.includes('localhost'))
 {
-  apiUrlDotNet="https://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/api/v1";
+  apiUrlDotNet="http://ehrmapi-env.eba-nm9akzrd.eu-north-1.elasticbeanstalk.com/api/v1";
 }
 export const environment = {
   production: true,
