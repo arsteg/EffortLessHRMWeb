1ef30e34069924e7805f26b829193c6dc0d99337
Mohamad Rafi <Mohamad.Rafi@pro-sight-vision.com>
2024-11-17 17:06:59 +0530

    added an environment file for test

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrlDotNet:`https://effortlesshrmapi-develop-eyd4cwezfvdebfbq.centralindia-01.azurewebsites.net/api/v1`,
+  webSocketUrl:`https://effortlesshrmapi-develop-eyd4cwezfvdebfbq.centralindia-01.azurewebsites.net/`
+};
