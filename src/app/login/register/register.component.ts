cacde6b0096e087c70e4207e8df808c22e7933e3
ARSTEG SOLUTIONS <info@arsteg.com>
2024-11-17 15:22:21 +0530

    Register page UI with OTP generator and verification

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -4,6 +4,7 @@ import { AuthenticationService } from 'src/app/_services/authentication.service'
 import { signup, webSignup } from '../../models/user'
 import { NotificationService } from '../../_services/notification.service'
 import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -14,21 +15,88 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class RegisterComponent implements OnInit {
   loading = false;
   registerNewUser: FormGroup;
+  otpGenerated: boolean = false;
+  otp: string = '';
+  otpVerified: boolean = false;
 
   constructor(private authenticationService: AuthenticationService, private router: Router,
     private notifyService: NotificationService,
+    private toast: ToastrService,
     private fb: FormBuilder) {
     this.registerNewUser = this.fb.group({
       firstName: ['', Validators.required],
       lastName: [''],
       email: [''],
-      password: [''],
-      passwordConfirm: [''],
+      otp: [''],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      passwordConfirm: ['', [Validators.required, Validators.minLength(8)]],
       companyName: ['']
-    })
+    }, { validator: this.passwordMatchValidator })
   }
 
   ngOnInit(): void {
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('passwordConfirm')?.value;
+    return password === confirmPassword ? null : { notMatching: true };
+  }
+
+  onGenerateOTP() {
+    let email = this.registerNewUser.get('email').value;
+    this.loading = true;
+    this.authenticationService.generateOTP({ email }).subscribe(
+      () => {
+        this.toast.success('OTP sent to your email', 'Success');
+        this.loading = false;
+        this.otpGenerated = true;
+
+      },
+      (error) => {
+        this.notifyService.showError(error.message, 'Error');
+      }
+    );
+  }
+
+  onReGenerateOTP() {
+    this.otpVerified = false;
+    this.loading = true;
+    this.onGenerateOTP();
+  }
+
+  onVerifyOTP() {
+    const payload = {
+      email: this.registerNewUser.get('email').value,
+      otp: this.registerNewUser.get('otp').value,
+    };
+    this.authenticationService.verifyOTP(payload).subscribe(
+      () => {
+        this.notifyService.showSuccess('OTP verified successfully', 'Success');
+        this.otpVerified = true;
+      },
+      (error) => {
+        this.notifyService.showError(error.message, 'Error');
+      }
+    );
+  }
+
+  onCancelOTP() {
+    const payload = {
+      email: this.registerNewUser.get('email').value,
+      otp: this.registerNewUser.get('otp').value,
+    };
+    this.authenticationService.cancelOTP(payload).subscribe(
+      () => {
+        this.notifyService.showSuccess('OTP cancelled', 'Success');
+        this.otpGenerated = false;
+        this.otpVerified = false;
+        this.registerNewUser.get('otp').reset();
+      },
+      (error) => {
+        this.notifyService.showError(error.message, 'Error');
+      }
+    );
   }
 
   onSubmit() {
@@ -53,4 +121,8 @@ export class RegisterComponent implements OnInit {
       }
     );
   }
+  getCurrentYear(): number {
+    return new Date().getFullYear();
+  }
+  
 }
