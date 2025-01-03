b2d935133e1abdbb9c3e466d5f74f06d441fdcec
ARSTEG SOLUTIONS <info@arsteg.com>
2025-01-03 11:00:40 +0530

    Change the web URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlDotNet:`https://effortlesshrmapi.azurewebsites.net/api/v1`,
-  webSocketUrl:`https://effortlesshrmapi.azurewebsites.net`
+  apiUrlDotNet:`https://effortlesshrm-e029cd6a5095.herokuapp.com/api/v1`,
+  webSocketUrl:`https://effortlesshrm-e029cd6a5095.herokuapp.com`
 };
