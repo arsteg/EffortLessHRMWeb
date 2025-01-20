59e121f6638799a1e4db0b0f17f5cafdcd59b54f
ARSTEG SOLUTIONS <info@arsteg.com>
2025-01-20 13:12:53 +0530

    Create environment.test.ts

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrlDotNet:`https://effortlesshrmapi.azurewebsites.net/api/v1`,
+  webSocketUrl:`https://effortlesshrmapi.azurewebsites.net/`
+};
