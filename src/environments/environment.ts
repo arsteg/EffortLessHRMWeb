80d099b814b0f88c193f160a5595f8fc87cfab57
ARSTEG SOLUTIONS <info@arsteg.com>
2025-03-17 15:25:49 +0530

    added web socket port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrlDotNet:`http://localhost:8080/api/v1`,
-  webSocketUrl:`http://localhost:8090`
+  webSocketUrl:`http://localhost:8080`
 };
 
