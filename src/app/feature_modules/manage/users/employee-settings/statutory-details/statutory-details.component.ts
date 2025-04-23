72fb56dd82f398ea288fbf58b82b8b329992e6c7
ARSTEG SOLUTIONS <info@arsteg.com>
2025-04-23 19:14:27 +0530

    Added validations for gratuity Flag in salary structure

--- src/app/feature_modules/manage/users/employee-settings/statutory-details/statutory-details.component.ts
+++ src/app/feature_modules/manage/users/employee-settings/statutory-details/statutory-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { UserService } from 'src/app/_services/users.service';
 import { forkJoin } from 'rxjs';
+import { PayrollService } from 'src/app/_services/payroll.service';
 
 @Component({
   selector: 'app-statutory-details',
@@ -13,12 +14,13 @@ import { forkJoin } from 'rxjs';
 export class StatutoryDetailsComponent {
   statutoryDetailsForm: FormGroup;
   selectedUser: any;
-
+  generalSettings: any;
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
     private toast: ToastrService,
-    private router: Router
+    private router: Router,
+    private payrollService: PayrollService
   ) {
     this.statutoryDetailsForm = this.fb.group({
       user: [''],
@@ -49,7 +51,12 @@ export class StatutoryDetailsComponent {
   ngOnInit() {
     this.logUrlSegmentsForUser();
   }
-
+  ngAfterViewInit() {
+    // Set gratuity validation after data is fully loaded
+    setTimeout(() => {
+      this.setGratuityEligibilityValidator();
+    }, 1000);
+  }
   logUrlSegmentsForUser() {
     const urlPath = this.router.url;
     const segments = urlPath.split('/').filter(segment => segment);
@@ -57,6 +64,9 @@ export class StatutoryDetailsComponent {
       const employee = segments[segments.length - 3];
       this.userService.getUserByEmpCode(employee).subscribe((res: any) => {
         this.selectedUser = res.data;
+        this.payrollService.getGeneralSettings(this.selectedUser[0]?.company?._id).subscribe((res: any) => {
+          this.generalSettings = res.data;
+        });
         this.getStatutoryDetailsByUser();
       })
     }
@@ -74,6 +84,8 @@ export class StatutoryDetailsComponent {
     this.statutoryDetailsForm.value.user = this.selectedUser[0].id;
     this.statutoryDetailsForm.value.taxRegimeUpdatedBy = this.selectedUser[0]?.id
     this.userService.getStatutoryByUserId(this.selectedUser[0].id).subscribe((res: any) => {
+      this.statutoryDetailsForm.get('isGratuityEligible').enable();
+      console.log(this.statutoryDetailsForm.value);
       if (res.data.length === 0) {
         this.userService.addStatutoryDetails(this.statutoryDetailsForm.value).subscribe((res: any) => {
           this.getStatutoryDetailsByUser();
@@ -82,13 +94,48 @@ export class StatutoryDetailsComponent {
           this.toast.error('Statutory Details Add Failed');
         })
       } else {
-        this.userService.updateStatutoryDetails(res.data[0]._id, this.statutoryDetailsForm.value).subscribe((res: any) => {
+        this.userService.updateStatutoryDetails(res.data[0]?._id, this.statutoryDetailsForm.value).subscribe((res: any) => {
           this.getStatutoryDetailsByUser();
           this.toast.success('Statutory Details Updated Successfully');
         }, error => {
           this.toast.error('Statutory Details Update Failed');
         })
       }
+      this.statutoryDetailsForm.get('isGratuityEligible').disable();
     });
+
   }
+  setGratuityEligibilityValidator() {
+    const gratuityControl = this.statutoryDetailsForm.get('isGratuityEligible');
+
+    if (!this.generalSettings?.isGraduityEligible) {
+      // Not eligible per general settings
+      gratuityControl?.setValue(false);
+      gratuityControl?.setErrors({ generalSettingRestricted: true });
+      gratuityControl?.disable();
+      return;
+    }
+
+    // Calculate year difference
+    const createdOn = new Date(this.selectedUser[0]?.createdOn);
+    const currentDate = new Date();
+    const yearDiff = currentDate.getFullYear() - createdOn.getFullYear();
+    const monthDiff = currentDate.getMonth() - createdOn.getMonth();
+    const dayDiff = currentDate.getDate() - createdOn.getDate();
+
+    const completedFiveYears = (
+      yearDiff > 5 ||
+      (yearDiff === 5 && (monthDiff > 0 || (monthDiff === 0 && dayDiff >= 0)))
+    );
+
+    if (!completedFiveYears) {
+      gratuityControl?.setValue(false);
+      gratuityControl?.setErrors({ notCompletedFiveYears: true });
+      gratuityControl?.disable();
+    } else {
+      gratuityControl?.enable();
+      gratuityControl?.setErrors(null);
+    }
+  }
+
 }
