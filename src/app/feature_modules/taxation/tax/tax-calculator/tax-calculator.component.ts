99b494bdc72f5549e012165a1e6ed0aadd9894da
ARSTEG SOLUTIONS <info@arsteg.com>
2025-05-09 19:46:56 +0530

    removed new regime tax calculations

--- src/app/feature_modules/taxation/tax/tax-calculator/tax-calculator.component.ts
+++ src/app/feature_modules/taxation/tax/tax-calculator/tax-calculator.component.ts
@@ -132,7 +132,7 @@ export class TaxCalculatorComponent implements OnInit {
         const totalDeductions = (this.hraVerifiedTotal || 0) + componentDeductions;
         this.taxableSalary = this.grossSalary - totalDeductions;
         this.taxPayableOldRegime = this.calculateOldRegimeTax(this.taxableSalary);
-        this.taxPayableNewRegime = this.calculateNewRegimeTax(this.taxableSalary);
+        // this.taxPayableNewRegime = this.calculateNewRegimeTax(this.taxableSalary);
     }
 }
 
@@ -150,25 +150,25 @@ export class TaxCalculatorComponent implements OnInit {
     return tax;
   }
 
-  calculateNewRegimeTax(taxableSalary: number): number {
-    let tax = 0;
-    if (taxableSalary <= 250000) {
-      tax = 0;
-    } else if (taxableSalary <= 500000) {
-      tax = (taxableSalary - 250000) * 0.05;
-    } else if (taxableSalary <= 750000) {
-      tax = 12500 + (taxableSalary - 500000) * 0.1;
-    } else if (taxableSalary <= 1000000) {
-      tax = 37500 + (taxableSalary - 750000) * 0.15;
-    } else if (taxableSalary <= 1250000) {
-      tax = 75000 + (taxableSalary - 1000000) * 0.2;
-    } else if (taxableSalary <= 1500000) {
-      tax = 125000 + (taxableSalary - 1250000) * 0.25;
-    } else {
-      tax = 187500 + (taxableSalary - 1500000) * 0.3;
-    }
-    return tax;
-  }
+  // calculateNewRegimeTax(taxableSalary: number): number {
+  //   let tax = 0;
+  //   if (taxableSalary <= 250000) {
+  //     tax = 0;
+  //   } else if (taxableSalary <= 500000) {
+  //     tax = (taxableSalary - 250000) * 0.05;
+  //   } else if (taxableSalary <= 750000) {
+  //     tax = 12500 + (taxableSalary - 500000) * 0.1;
+  //   } else if (taxableSalary <= 1000000) {
+  //     tax = 37500 + (taxableSalary - 750000) * 0.15;
+  //   } else if (taxableSalary <= 1250000) {
+  //     tax = 75000 + (taxableSalary - 1000000) * 0.2;
+  //   } else if (taxableSalary <= 1500000) {
+  //     tax = 125000 + (taxableSalary - 1250000) * 0.25;
+  //   } else {
+  //     tax = 187500 + (taxableSalary - 1500000) * 0.3;
+  //   }
+  //   return tax;
+  // }
 
   initializeSectionControls() {
     this.taxSections.forEach(section => {
