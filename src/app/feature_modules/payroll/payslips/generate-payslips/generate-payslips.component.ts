dbeec2735fb79ce7b3d7fe0a398f2c4d761beb9e
ARSTEG SOLUTIONS <info@arsteg.com>
2025-05-28 17:36:00 +0530

    added salary slips

--- src/app/feature_modules/payroll/payslips/generate-payslips/generate-payslips.component.ts
+++ src/app/feature_modules/payroll/payslips/generate-payslips/generate-payslips.component.ts
@@ -28,19 +28,40 @@ export class GeneratePayslipsComponent {
   calculateTotals(): void {
     const ps = this.payslip;
 
+    // Calculate total earnings
     const fixed = ps?.PayrollUser?.totalFixedAllowance || 0;
     const flexi = ps?.PayrollUser?.totalFlexiBenefits || 0;
     const variable = ps?.PayrollUser?.totalVariableAllowance || 0;
-    const finalOvertime = ps?.finalOvertime || 0;
+    const finalOvertime = ps?.latestOvertime?.OvertimeAmount || 0;
 
-    this.totalEarnings = fixed + flexi + variable + finalOvertime;
+    // Sum amounts for Disbursement loans
+    const loanDisbursement = Array.isArray(ps?.allLoanAdvances)
+      ? ps.allLoanAdvances.reduce((sum: number, loan: any) => {
+        return loan.type === 'Disbursement' ? sum + (loan.amount || 0) : sum;
+      }, 0)
+      : 0;
+    const totalArrears = Array.isArray(ps?.manualArrears)
+      ? ps.manualArrears.reduce((sum: number, arrears: any) => {
+        return sum + (arrears.amount || 0);
+      }, 0)
+      : 0;
 
+    this.totalEarnings = fixed + flexi + variable + finalOvertime + loanDisbursement + totalArrears;
+
+    // Calculate total deductions
     const fixedDeduction = ps?.PayrollUser?.totalFixedDeduction || 0;
     const variableDeduction = ps?.PayrollUser?.totalVariableDeduction || 0;
-    const incomeTax = ps?.PayrollUser?.tdsCalculated || 0;
-    const loanAdvance = ps?.PayrollUser?.totalLoanRepayment || 0;
-    
-    this.totalDeductions = fixedDeduction + variableDeduction + incomeTax + loanAdvance;
+    const incomeTax = ps?.tdsCalculated || 0;
+
+    // Sum amounts for Repayment loans
+    const loanRepayment = Array.isArray(ps?.allLoanAdvances)
+      ? ps.allLoanAdvances.reduce((sum: number, loan: any) => {
+        return loan.type === 'Repayment' ? sum + (loan.amount || 0) : sum;
+      }, 0)
+      : 0;
+
+    console.log(fixedDeduction, variableDeduction, incomeTax, loanRepayment);
+    this.totalDeductions = fixedDeduction + variableDeduction + incomeTax + loanRepayment;
   }
 
   getCompanyNameFromCookies(): string | null {
