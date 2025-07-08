fe138b21f1d8767317bb01e153f3a64c061d0013
Sandeep Kumar <83569152+arsteg@users.noreply.github.com>
2025-07-08 21:29:30 +0530

    leave balance, leave application UI Design under leave
    leave balance, leave application UI Design under leave

--- src/app/feature_modules/leave/leave-application/pending/pending-application.component.ts
+++ src/app/feature_modules/leave/leave-application/pending/pending-application.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LeaveService } from 'src/app/_services/leave.service';
 
 @Component({
   selector: 'app-pending-application',
@@ -15,9 +17,22 @@ export class PendingApplicationComponent {
     view: true
   };
 
- @Input() selectedTab: number;
+ //@Input() selectedTab: number;
+ selectedTab: number = Number(localStorage.getItem('leaveSelectedTab')) || 1;
+  private tabSubscription: Subscription;
+
+  constructor(private leaveService: LeaveService){}
 
  ngOnInit(){
+  this.tabSubscription = this.leaveService.getSelectedTab().subscribe(tab => {
+      this.selectedTab = tab;
+  });  
  }
+
+ ngOnDestroy(): void {
+    if (this.tabSubscription) {
+      this.tabSubscription.unsubscribe();
+    }
+  }
 
 }
