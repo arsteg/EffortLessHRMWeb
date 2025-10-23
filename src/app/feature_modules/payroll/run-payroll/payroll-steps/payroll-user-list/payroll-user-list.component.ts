1c97a9f97c15bc54767b99ba29e9770c669ce2fb
rukhsana <rukhsana18arsteg@gmail.com>
2025-10-23 19:25:45 +0530

    added language translation

--- src/app/feature_modules/payroll/run-payroll/payroll-steps/payroll-user-list/payroll-user-list.component.ts
+++ src/app/feature_modules/payroll/run-payroll/payroll-steps/payroll-user-list/payroll-user-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { CommonService } from 'src/app/_services/common.Service';
 import { PayrollService } from 'src/app/_services/payroll.service';
 
 @Component({
