dcc04ff543ebe336e193d058360356731c6868e3
Sandeep Kumar <sandeepkumar.chipsoft@gmail.com>
2026-04-10 15:34:46 +0530

    added test enquiry form
    added test enquiry form

---
+++ src/app/enquiry/test-enquiry/test-enquiry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-enquiry',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-enquiry.component.html',
+  styleUrl: './test-enquiry.component.css'
+})
+export class TestEnquiryComponent {
+
+}
