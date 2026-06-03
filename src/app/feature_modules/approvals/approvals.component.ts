694a7b1608d2b12e3d8608ffc215dd0ff9e86996
Sandeep Kumar <sandeepkumar.chipsoft@gmail.com>
2026-06-03 20:12:13 +0530

    manual time redirection
    manual time redirection

--- src/app/feature_modules/approvals/approvals.component.ts
+++ src/app/feature_modules/approvals/approvals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-approvals',
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class ApprovalsComponent implements OnInit {
   selectedTab: number = 0;
   constructor(
+    private route: ActivatedRoute
     ) { }
 
-  ngOnInit(): void {  
+  ngOnInit(): void {
+    // Check for tab query parameter to auto-select the correct tab
+    this.route.queryParams.subscribe(params => {
+      if (params['tab'] === 'manual-time') {
+        this.selectedTab = 1; // Manual Time tab index
+      } else if (params['tab'] === 'productivity') {
+        this.selectedTab = 0; // Productivity Apps tab index
+      }
+    });
   }
   selectTab(tabIndex: number) {
     this.selectedTab = tabIndex;
