2302242fc221275379de75985e50925cba791ccb
Sandeep Kumar <sandeepkumar.chipsoft@gmail.com>
2026-06-30 13:35:10 +0530

    updated environment variable url
    updated environment variable url

--- src/environments/environment.Development.ts
+++ src/environments/environment.Development.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrlDotNet:`https://effortlesshrmtest-190a3bd02733.herokuapp.com/api/v1`,
-  webSocketUrl:`https://effortlesshrmtest-190a3bd02733.herokuapp.com/`
+  // apiUrlDotNet:`https://effortlesshrmtest-190a3bd02733.herokuapp.com/api/v1`,
+  // webSocketUrl:`https://effortlesshrmtest-190a3bd02733.herokuapp.com/`
+  apiUrlDotNet:`https://api.effortlesshrm.com/api/v1`,
+  webSocketUrl:`https://api.effortlesshrm.com/`
 };
